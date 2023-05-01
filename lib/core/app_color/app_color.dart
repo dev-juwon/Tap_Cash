16e23fb45d3ad8187f6a1ec2086a10ac78dbb6fe
HusseinMohamed <husseinhtm99@gmail.com>
2023-05-01 23:28:28 +0300

    Complete OnBoarding Screen
    Signed-off-by: HusseinMohamed <husseinhtm99@gmail.com>

---
+++ lib/core/app_color/app_color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primaryColor = Color(0xFF06C258);
+  static const greyColor = Color(0xFFD0D5DD);
+  static const titleColor = Color(0xFF475467);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tap_cash/core/app_string/app_string.dart';
-import 'package:tap_cash/presentation/screens/splash_screen.dart';
+import 'package:tap_cash/core/network/app_constant.dart';
+import 'package:tap_cash/core/network/cache_helper.dart';
+import 'package:tap_cash/presentation/screens/on_board_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   ScreenUtil.ensureScreenSize();
+
+  await CacheHelper.init();
+
+  AppConstant.uId = CacheHelper.getData(key: 'uId');
+  if (kDebugMode) {
+    print('User ID = ${AppConstant.uId}');
+  }
   runApp(const MyApp());
 }
 
@@ -22,7 +33,7 @@ class MyApp extends StatelessWidget {
           DeviceOrientation.portraitDown,
         ]);
         return ScreenUtilInit(
-          designSize: const Size(375, 800),
+          designSize: const Size(360, 690),
           minTextAdapt: true,
           splitScreenMode: true,
           builder: (context, child) {
@@ -31,7 +42,7 @@ class MyApp extends StatelessWidget {
               title: AppString.appTitle,
               theme: ThemeData.dark()
                   .copyWith(scaffoldBackgroundColor: Colors.white),
-              home: const SplashScreen(),
+              home: const OnBoardingScreen(),
             );
           },
         );
