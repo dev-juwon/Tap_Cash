ac4c08ba08577f117cbaf0dd72bcf2c5d2dca0db
HusseinMohamed <husseinhtm99@gmail.com>
2023-05-10 07:38:38 +0300

    Complete Splash Screen Complete OnBoarding Screen Complete SignIn Screen Complete SignUp Screen Complete Otp Screen Complete ChangePass Screen Complete ForgetPass Screen Theme Data Home Screen
    Signed-off-by: HusseinMohamed <husseinhtm99@gmail.com>

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -2,22 +2,167 @@
 class Assets {
   Assets._();
 
+  static const String assetsChoose
+
+  method
+
+  =
+
+  '
+
+  assets/choose method.jpg
+
+  ';
+
+  static const String assetsHome = 'assets/Home.jpg';
+  static const String assetsIPhone
+
+  13
+
+  mini
+
+  1
+
+  =
+
+  '
+
+  assets/iPhone 13 mini - 1.jpg
+
+  ';
+
+  static const String assetsIPhone
+
+  13
+
+  mini
+
+  10
+
+  =
+
+  '
+
+  assets/iPhone 13 mini - 10.jpg
+
+  ';
+
+  static const String assetsIPhone
+
+  13
+
+  mini
+
+  2
+
+  =
+
+  '
+
+  assets/iPhone 13 mini - 2.jpg
+
+  ';
+
+  static const String assetsIPhone
+
+  13
+
+  mini
+
+  7
+
+  =
+
+  '
+
+  assets/iPhone 13 mini - 7.jpg
+
+  ';
+
+  static const String assetsIPhone
+
+  13
+
+  mini
+
+  8
+
+  =
+
+  '
+
+  assets/iPhone 13 mini - 8.jpg
+
+  ';
+
+  static const String assetsIPhone
+
+  13
+
+  mini
+
+  9
+
+  =
+
+  '
+
+  assets/iPhone 13 mini - 9.jpg
+
+  ';
+
   static const String assetsImagesCard = 'assets/images/card.svg';
+  static const String assetsImagesEmptyWallet = 'assets/images/empty-wallet.svg';
+  static const String assetsNew
+
+  pass
+
+  =
+
+  '
+
+  assets/new pass.jpg
+
+  ';
+
+  static const String assetsOtp = 'assets/otp.jpg';
+  static const String assetsOtp1 = 'assets/otp-1.jpg';
+  static const String assetsSign
+
+  in
+
+  =
+
+  '
+
+  assets/Sign in.jpg
+
+  ';
+
+  static const String assetsSign
+
+  up
+
+  =
+
+  '
+
+  assets/sign up.jpg
+
+  ';
+
   static const String imagesArrowBack = 'assets/images/arrow_back.svg';
   static const String imagesBill = 'assets/images/bill.svg';
   static const String imagesCalendar = 'assets/images/calendar.png';
+  static const String imagesCard = 'assets/images/card.png';
   static const String imagesChane = 'assets/images/chane.svg';
   static const String imagesEmptyWallet = 'assets/images/empty-wallet.png';
-  static const String imagesEmptyWalletAdd =
-      'assets/images/empty-wallet-add.svg';
-  static const String imagesEmptyWalletChange =
-      'assets/images/empty-wallet-change.svg';
-  static const String imagesForgotPassword =
-      'assets/images/Forgot_password.svg';
+  static const String imagesEmptyWalletAdd = 'assets/images/empty-wallet-add.svg';
+  static const String imagesEmptyWalletChange = 'assets/images/empty-wallet-change.svg';
+  static const String imagesForgotPassword = 'assets/images/Forgot_password.svg';
   static const String imagesGraph = 'assets/images/graph.png';
   static const String imagesHistory = 'assets/images/History.svg';
-  static const String imagesLoginBackGround =
-      'assets/images/login_backGround.svg';
+  static const String imagesLoginBackGround = 'assets/images/login_backGround.svg';
   static const String imagesMore = 'assets/images/more.svg';
   static const String imagesOnBoard1 = 'assets/images/on_board1.svg';
   static const String imagesOnBoard2 = 'assets/images/on_board2.svg';
@@ -28,4 +173,5 @@ class Assets {
   static const String imagesSplashScreen = 'assets/images/splash_screen.png';
   static const String imagesTecnoPattern = 'assets/images/TecnoPattern.svg';
   static const String imagesUser = 'assets/images/user.png';
+
 }
